fc12a7ad9653d459ce1b8588098e76174f291e94
silvanos <ericoderito@gmail.com>
2022-07-24 09:20:44 +0300

    add interactivity

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-import './style.css'
+import "./style.css";
+
+// display overlay when one hovers over the card image container
+
+// card image cotainer
+const imgContainer = document.getElementById("img-container");
+// overlay
+const overlay = document.getElementById("overlay");
+
+// add event listeners to imgContainer
+// turn on visibility when mouse hovers over the container
+imgContainer.addEventListener("mouseenter", () => {
+  overlay.style.visibility = "visible";
+});
+
+// turn off visiblity when mouse leaves the container
+imgContainer.addEventListener("mouseleave", () => {
+  overlay.style.visibility = "hidden";
+})
